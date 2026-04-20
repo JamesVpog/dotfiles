9afa98e37ebe17d252d442ee4a410cd6b588b50b
James Vongphasouk <james.vongpha@gmail.com>
2026-04-19 20:57:15 -0600

    feat: add wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,49 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 16
+config.font = wezterm.font('JetBrains Mono' )
+config.color_scheme = 'catppuccin-macchiato'
+
+
+config.enable_tab_bar = false
+
+local dimmer = { brightness = 0.15 }
+
+local ekko_bg = wezterm.home_dir .. "/ekko2.png"
+
+config.background = {
+	  {
+	    source = {
+	      File = ekko_bg,
+	    },
+	    hsb = dimmer,
+	    height="Cover",
+	    width="Cover",
+	    repeat_x="NoRepeat",
+	    -- vertical_offset="10%",
+	    -- horizontal_offset="10%",
+	  }
+}
+
+
+
+
+
+config.keys = {
+  -- CTRL-SHIFT-l activates the debug overlay
+  { key = 'L', mods = 'CTRL', action = wezterm.action.ShowDebugOverlay },
+}
+
+-- Finally, return the configuration to wezterm:
+return config
